9869bd4148831eb238bd1810b83f00234acaa822
karlshaver <karl@meshfresh.com>
2018-02-13 11:57:11 -0500

    Corrects ACF Options page

--- functions.php
+++ functions.php
@@ -6,6 +6,18 @@ include('functions/start.php');
 
 include('functions/clean.php');
 
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+   
+}
+
 //Custon wp-admin logo
 function my_custom_login_logo() {
   echo '<style type="text/css">
