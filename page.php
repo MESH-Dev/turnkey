2e8f237d1ee6b0389a109bc602bb07ae302ca1c5
karlshaver <karl@meshfresh.com>
2018-02-15 17:53:27 -0500

    Fixes page functions

--- page.php
+++ page.php
@@ -7,35 +7,32 @@
 	  if (have_rows('page_columns')):
 		  $rows = get_field('page_columns');
 			$row_count = count($rows);
-			if($row_count == 1){
-				 $classes= 'class="content columns-12"';
-			} elseif($row_count == 2){
-			  $classes='class="content columns-6"';
-		  }
+			?>
+			<main class="main-content" id="content">
+				<?php
+				$callout = get_field('page_callout');
+				if ($callout) {
+					?>
+					<h4 class="page-callout"><?php the_field('page_callout'); ?></h1>
+						<hr>
+					<?php
+				}
+				if($row_count == 1){
+					 $classes= 'class="content columns-12"';
+				} elseif($row_count == 2){
+				  $classes='class="content columns-6"';
+			  }
 				 while(have_rows('page_columns')):the_row();
 			//         // display a sub field value
-					//   $content = the_sub_field('column_content');
-					  ?>
-					  <main class="main-content" id="content">
-							  <?php
-							  $callout = get_field('page_callout');
-							  if ($callout) {
-								  ?>
-								  <h4 class="page-callout"><?php the_field('page_callout'); ?></h1>
-									  <hr>
-								  <?php
-							  }
-							  ?>
+					//   $content = the_sub_field('column_content'); ?>
 						  <div <?php echo $classes ?>>
 							<?php the_sub_field('column_content'); ?>
 						  </div>
-					  </main>
 					  <?php
-				 endwhile;
-			else :
+				 endwhile;?>
+				 </main>
 				 // no rows found
-			endif;
-			?>
+			<?php endif; ?>
 </div>
 
 <!-- <main id="content">
