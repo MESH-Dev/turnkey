230f12a52429fb4c0293e208e60415dd1818f96e
karlshaver <karl@meshfresh.com>
2018-02-20 16:16:43 -0500

    Fixes broken Instagram access token

--- js/mesh.js
+++ js/mesh.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($){
        get: 'user',
       //  clientId: 'd346c1de6d274f4994f9345b72b3a633',
        userId: 6209152766,
-       accessToken: '6209152766.1677ed0.0229100bf6574435933030af71efe958',
+       accessToken: '6209152766.1677ed0.9b724147d01e451b9c6e0ace95fa66f5',
        template: '<div class="half ig-card"><div class="instagram-image"><a href="{{link}}"><img src="{{image}}" /></a></div><div class="instagram-caption"><p>{{caption}}</p></div></div>',
        resolution: 'standard_resolution'
    });
