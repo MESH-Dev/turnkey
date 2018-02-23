78565845f200a86d46e89a750ae45a1be26b8162
karlshaver <karl@meshfresh.com>
2018-02-23 16:02:12 -0500

    Styling for example content, changes to example global elements

--- footer.php
+++ footer.php
@@ -3,16 +3,16 @@
 <footer class="site-footer">
    <div class="cards fixed left">
       <p>
-         311 Washington Street, West<br>Charleston, WV  25302
-         <a class="info-item" href="tel:681-265-9468">681-265-9468</a>
-         <a class="info-item" href="mailto:contact@elkcityrecords.com">contact@elkcityrecords.com</a>
+         311 Example Street, West<br>Charleston, WV  12345
+         <a class="info-item" href="tel:123-456-1234">123-456-1234</a>
+         <a class="info-item" href="mailto:example@example.com">example@example.com</a>
       </p>
    </div>
    <div class="cards fixed right">
       <div class="footer social">
-        <a href="https://www.facebook.com/elkcityrecords/" target="_blank"><i class="fa fa-lg fa-facebook-square"></i></a>
-        <a href="https://www.instagram.com/ecrecords/" target="_blank"><i class="fa fa-lg fa-instagram"></i></a>
-        <a href="https://www.yelp.com/biz/elk-city-records-charleston" target="_blank"><i class="fa fa-lg fa-yelp"></i></a>
+        <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-lg fa-facebook-square"></i></a>
+        <a href="https://www.instagram.com/" target="_blank"><i class="fa fa-lg fa-instagram"></i></a>
+        <a href="https://www.yelp.com/" target="_blank"><i class="fa fa-lg fa-yelp"></i></a>
       </div>
      <p><span class="mesh info-item">Website by <a href="http://meshfresh.com" target="_blank">MESH</a></span></p>
    </div>
