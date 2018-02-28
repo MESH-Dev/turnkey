834189109c5e95fb827403282457e6fcfa8e0425
karlshaver <karl@meshfresh.com>
2018-02-28 15:26:58 -0500

    Changes Instagram integration to MESH account

--- js/mesh.js
+++ js/mesh.js
@@ -10,8 +10,8 @@ jQuery(document).ready(function($){
    var userFeed = new Instafeed({
        get: 'user',
       //  clientId: 'd346c1de6d274f4994f9345b72b3a633',
-       userId: 6209152766,
-       accessToken: '6209152766.1677ed0.9b724147d01e451b9c6e0ace95fa66f5',
+       userId: 1167443738,
+       accessToken: '1167443738.1677ed0.8626f5a5334746de8f919ce75757d845',
        template: '<div class="half ig-card"><div class="instagram-image"><a href="{{link}}"><img src="{{image}}" /></a></div><div class="instagram-caption"><p>{{caption}}</p></div></div>',
        resolution: 'standard_resolution'
    });
