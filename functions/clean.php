92444f35ac2e432812f9bf7717c37958c2bfc560
karlshaver <karl@meshfresh.com>
2018-04-05 09:29:38 -0400

    Dashboard clean, social hovers

--- functions/clean.php
+++ functions/clean.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 //Use this file to clean up wp admin dashboard ============
 
 
@@ -29,11 +29,11 @@ function delete_menu_items() {
     /*** Remove menu http://codex.wordpress.org/Function_Reference/remove_menu_page
     syntaxe : remove_menu_page( $menu_slug )  **/
     //remove_menu_page('index.php'); // Dashboard
-    //remove_menu_page('edit.php'); // Posts
+    remove_menu_page('edit.php'); // Posts
     //remove_menu_page('upload.php'); // Media
-    //remove_menu_page('link-manager.php'); // Links
+    remove_menu_page('link-manager.php'); // Links
     //remove_menu_page('edit.php?post_type=page'); // Pages
-    //remove_menu_page('edit-comments.php'); // Comments
+    remove_menu_page('edit-comments.php'); // Comments
     //remove_menu_page('themes.php'); // Appearance
     //remove_menu_page('plugins.php'); // Plugins
     //remove_menu_page('users.php'); // Users
